449c0f5cf69fe39174cf88a15927b5cba272940b
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-04-25 17:31:34 +0530

    basic structure of the program

---
+++ rct-update-array-objects/src/App.jsx
@@ -0,0 +1,12 @@
+/* how to update state of an array of objects in react */
+import MyComponent from './MyComponent.jsx'
+function App() {
+  
+  return (
+    <>
+      <MyComponent/>
+    </>
+  )
+}
+
+export default App
---
+++ rct-update-array-objects/src/MyComponent.jsx
@@ -0,0 +1,34 @@
+/* how to update state of an array of objects 
+in react */
+import React, {useState} from 'react';
+
+function MyComponent() {
+
+    const [cars, setCars] = useState([]);
+    /* to set the current use new Date().getFullYear() method */
+    const [carYear, setCarYear] = useState(new Date().getFullYear());
+    const [carMake, setCarMake] = useState("");
+    const [carModel, setCarModel] = useState("");
+
+    return(
+        <div> 
+            <h2> list of car objects</h2>
+            <ul> 
+
+            </ul>
+            <input type="number" 
+                   value={carYear} /> <br />
+            <input type="text" 
+                   value={carMake} /> <br />
+            <input type="text"
+                    value={carModel} /> <br />
+            <button> add car </button>
+        </div>
+    );
+}
+export default MyComponent
+
+
+
+
+
